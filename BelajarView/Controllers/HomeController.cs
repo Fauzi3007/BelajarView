98bb5c15625d559e2422a8b8e3a237e5efde0537
FauziIsyrinApridal <fauzi.isyrin@citiasiainc.id>
2025-11-28 11:16:49 +0700

    Selesai .NET View

--- BelajarView/Controllers/HomeController.cs
+++ BelajarView/Controllers/HomeController.cs
@@ -8,11 +8,20 @@ namespace BelajarView.Controllers
     {
         public ViewResult Index()
         {
-            return View();
+            List<Student> listStudents = new List<Student>()
+            {
+               new Student() { Id = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { Id = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+               new Student() { Id = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { Id = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+               new Student() { Id = 105, Name = "Pam", Branch = "ETC", Section = "B", Gender = "Female" }
+            };
+            return View(listStudents);
         }
-        public ViewResult Details()
+        public ViewResult Details(int Id)
         {
-            return View();
+            var studentDetails = new Student() { Id = Id, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" };
+            return View(studentDetails);
         }
         public ViewResult Privacy()
         {
---
+++ BelajarView/ViewComponents/TopProductsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using BelajarView.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace ViewComponentInMVC.ViewComponents
+{
+    public class TopProductsViewComponent : ViewComponent
+    {
+        //The Invoke method for the View component
+        public async Task<IViewComponentResult> InvokeAsync(int count)
+        {
+            // Your logic for preparing data
+            ProductRepository productRepository = new ProductRepository();
+            var products = await productRepository.GetTopProductsAsync(count);
+            return View(products);
+        }
+
+        //public IViewComponentResult Invoke(int count)
+        //{
+        // // Your logic for preparing data
+        // ProductRepository productRepository = new ProductRepository();
+        // var products = productRepository.GetTopProductsAsync(count).Result;
+        // return View(products);
+        //}
+    }
+}
